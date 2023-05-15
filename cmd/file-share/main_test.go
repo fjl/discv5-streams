14b8aff50d6f6fc3c973ac8a7c6b57f64504b7ee
Felix Lange <fjl@twurst.com>
2023-05-15 11:26:18 +0200

    cmd/file-share: add test that instantiates app state

---
+++ cmd/file-share/main_test.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestAppStateSetup(t *testing.T) {
+	tmp := t.TempDir()
+	state := newAppState(tmp)
+	defer state.Close()
+
+	var (
+		start       = time.Now()
+		networkUp   bool
+		transfersUp bool
+		filesUp     bool
+	)
+
+	// Wait for network.
+	for !networkUp && time.Since(start) < 2*time.Second {
+		s := state.net.State()
+		switch {
+		case s.loading:
+			<-state.net.Changed()
+			continue
+		case s.startError != nil:
+			t.Fatal("Network start error:", s.startError)
+		default:
+			networkUp = true
+		}
+	}
+
+	// Wait for transfers.
+	for !transfersUp && time.Since(start) < 2*time.Second {
+		s := state.transfers.State()
+		switch {
+		case s.loading:
+			<-state.transfers.Changed()
+			continue
+		case s.loadError != nil:
+			t.Fatal("Transfers load error:", s.loadError)
+		default:
+			transfersUp = true
+		}
+	}
+
+	// Wait for files.
+	for !filesUp && time.Since(start) < 2*time.Second {
+		s := state.fs.State()
+		switch {
+		case s.loading:
+			<-state.fs.Changed()
+			continue
+		case s.loadError != nil:
+			t.Fatal("Files load error:", s.loadError)
+		default:
+			filesUp = true
+		}
+	}
+
+	if !networkUp {
+		t.Fatal("networkController did not start")
+	}
+	if !transfersUp {
+		t.Fatal("transfersController did not start")
+	}
+	if !filesUp {
+		t.Fatal("filesController did not start")
+	}
+}
